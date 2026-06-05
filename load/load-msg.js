0958a0304f8519d07cbe7fcf171ed9de279f7d21
githud33 <githud33@gmail.com>
2026-06-06 05:35:31 +0700

    Update load-msg.js

--- load/load-msg.js
+++ load/load-msg.js
@@ -1,4 +1,4 @@
 var script = document.createElement('script');
-script.src = 'https://githud33.github.io/message/folder/ข้อความข้างบน.js?t=' + new Date().getTime();
+script.src = 'https://githud33.github.io/message/folder/message-1.js?t=' + new Date().getTime();
 script.defer = true;
 document.head.appendChild(script);
