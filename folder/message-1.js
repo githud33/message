aa27f2264fd884b3e59f9cedb74ad3d3c4243315
githud33 <githud33@gmail.com>
2026-06-06 09:16:12 +0700

    Update message-1.js

--- folder/message-1.js
+++ folder/message-1.js
@@ -1,1 +1,11 @@
+// 1. สร้างข้อความดิบขึ้นมา
 var textNode = document.createTextNode("**** ดูไม่ได้ แจ้งได้ในกลุ่มนะครับ หรือติดต่อทาง inbox Messenger ทักแจ้งมา ตรงปุ่มติดต่อแอดมิน จะทำการแก้ไขให้ทันที เมื่อรับทราบ ****");
+
+// 2. ค้นหาแท็ก <script> ตัวล่าสุดที่กำลังทำงานอยู่ ณ จุดนั้นบนหน้าเว็บ
+var scripts = document.getElementsByTagName('script');
+var currentScript = scripts[scripts.length - 1];
+
+// 3. เอาข้อความไปวางลงตรงจุดนั้นทันที (แปะตรงไหน อยู่ตรงนั้นเลย)
+if (currentScript && currentScript.parentNode) {
+    currentScript.parentNode.insertBefore(textNode, currentScript);
+}
