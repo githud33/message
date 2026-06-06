15616176b7d3fc9c3f7e740f7ad63a2e4714487d
githud33 <githud33@gmail.com>
2026-06-06 08:39:32 +0700

    Update message-1.js

--- folder/message-1.js
+++ folder/message-1.js
@@ -1,8 +1,1 @@
-// 1. สร้างข้อความดิบขึ้นมา (ไม่มีการแต่งสีหรือขนาดใดๆ ทั้งสิ้น)
 var textNode = document.createTextNode("**** ดูไม่ได้ แจ้งได้ในกลุ่มนะครับ หรือติดต่อทาง inbox Messenger ทักแจ้งมา ตรงปุ่มติดต่อแอดมิน จะทำการแก้ไขให้ทันที เมื่อรับทราบ ****");
-
-// 2. สั่งให้ระบบค้นหาพิกัดกล่องใน Blogger ที่เราแปะสคริปต์นี้ไว้
-var currentScript = document.currentScript;
-
-// 3. เอาข้อความไปวางลงตรงจุดนั้นทันที
-currentScript.parentNode.insertBefore(textNode, currentScript);
