8093d835ab6d6c08d8a72f89b39ec38829db707e
githud33 <githud33@gmail.com>
2026-06-07 22:56:09 +0700

    Delete load/ข้อความโลโก้บน.js

--- load/ข้อความโลโก้บน.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
-script.src = 'https://githud33.github.io/message/folder/ข้อความโลโก้บน.js.js?t=' + new Date().getTime();
-script.defer = true;
-document.head.appendChild(script);
