00a14f1297b7d80033ebf9994dd3475d83c9d645
githud33 <githud33@gmail.com>
2026-06-07 22:50:18 +0700

    Add files via upload

---
+++ load/ข้อความโลโก้บน.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/message/folder/ข้อความโลโก้บน.js.js?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
