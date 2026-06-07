5cd260a3e180045478c08936722f840dcff0e086
githud33 <githud33@gmail.com>
2026-06-08 00:12:47 +0700

    Update ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js

--- ล้างแคช/ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js
+++ ล้างแคช/ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 // ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
 script.src = 'https://githud33.github.io/message/ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js?t=' + new Date().getTime();
-script.defer = true;
+
 document.head.appendChild(script);
