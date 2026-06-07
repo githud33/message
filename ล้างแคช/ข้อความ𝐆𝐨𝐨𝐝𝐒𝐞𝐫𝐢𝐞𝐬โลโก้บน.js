ee18b8b5d88d566b8a6e6563e2fe2009e6c21796
githud33 <githud33@gmail.com>
2026-06-07 23:42:55 +0700

    Create ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬โลโก้บน.js

---
+++ ล้างแคช/ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬โลโก้บน.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/message/ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬โลโก้บน.js?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
