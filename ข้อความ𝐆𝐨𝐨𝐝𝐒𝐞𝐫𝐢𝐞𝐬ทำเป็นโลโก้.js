214343ba88bee943aed7fa1a737b48ba5fed6558
githud33 <githud33@gmail.com>
2026-06-08 12:34:46 +0700

    Update ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js

--- ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js
+++ ข้อความ𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬ทำเป็นโลโก้.js
@@ -3,10 +3,10 @@
 𝐇𝐚𝐩𝐩𝐲 𝐍𝐞𝐰 𝐘𝐞𝐚𝐫 𝟐𝟎𝟐𝟔
  */
 // บรรทัดนี้สั่งให้มันรอกล่องในหน้าเว็บโหลดเสร็จก่อนค่อยทำ (เอาไว้กันเอ๋อเวลาวางสคริปต์ไว้บนสุด)
-// document.addEventListener("DOMContentLoaded", function() {
+ document.addEventListener("DOMContentLoaded", function() {
     
     // ตรงนี้คือเอาข้อความไปใส่ในกล่องชื่อ ID
     document.getElementById("NameTag").innerHTML = "𝐆𝐨𝐨𝐝 𝐒𝐞𝐫𝐢𝐞𝐬";
   
-// });
+ });
 
