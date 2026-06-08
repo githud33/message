b23f80e47edadc73ab8f2daadab3a71680234155
githud33 <githud33@gmail.com>
2026-06-08 15:23:45 +0700

    Add files via upload

---
+++ ล้างแคช/ข้อความข้างล่างโลโก้𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/message/ข้อความข้างล่างโลโก้𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
