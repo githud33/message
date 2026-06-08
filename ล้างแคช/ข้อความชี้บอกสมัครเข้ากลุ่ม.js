11681ab120c863dfb25824942e80605073dd81eb
githud33 <githud33@gmail.com>
2026-06-08 14:54:01 +0700

    Delete ล้างแคช/ข้อความชี้บอกสมัครเข้ากลุ่ม.js

--- ล้างแคช/ข้อความชี้บอกสมัครเข้ากลุ่ม.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
-script.src = 'https://githud33.github.io/message/ไฟล์หลัก/ข้อความชี้บอกสมัครเข้ากลุ่ม.js?t=' + new Date().getTime();
-script.defer = true;
-document.head.appendChild(script);
